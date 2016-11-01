6c4dea1ba3364e6b8a7f932495a34232e3659cb3
hanmz <hanmz@fxiaoke.com>
2016-11-01 12:01:23 +0800

    test dubbo

--- src/test/java/com/hanmz/service/dubbo/ProviderTest.java
+++ src/test/java/com/hanmz/service/dubbo/ProviderTest.java
@@ -7,7 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class ProviderTest {
   public static void main(String[] args) throws Exception {
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-dubbo-provider.xml");
+    ClassPathXmlApplicationContext context =
+      new ClassPathXmlApplicationContext("classpath:spring/spring-dubbo-provider.xml");
     context.start();
 
     System.in.read(); // 按任意键退出
